7edc5d6a3bde00cb312e221b4bcc60428ea0069c
Carlos Henrique <chenrilima.ch@gmail.com>
2021-02-24 07:40:16 -0300

    Initial commit

---
+++ lib/models/order.dart
@@ -0,0 +1,16 @@
+import 'package:restaurant_food/models/food.dart';
+import 'package:restaurant_food/models/restaurant.dart';
+
+class Order {
+  final Restaurant restaurant;
+  final Food food;
+  final String date;
+  final int quantity;
+
+  Order({
+    this.date,
+    this.restaurant,
+    this.food,
+    this.quantity,
+  });
+}
